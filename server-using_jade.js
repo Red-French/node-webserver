ae2577b87acd07c68346a580ac6ec1afcd56eecc
Red-French <red@redfrench.net>
2016-02-04 10:40:46 -0600

    added a form

--- server-using_jade.js
+++ server-using_jade.js
@@ -3,61 +3,57 @@
 'use strict';
 
 const app = require('express')();
+const bodyParser = require('body-parser'); // has to be installed with npm
+
 // const http = require('http');
 const PORT = process.env.PORT || 3000;  // if I don't pass a port, then port 3000
 
 // JADE SETUP
-app.set('view engine', 'jade'); // allows to use jade
+app.set('view engine', 'jade'); // allows to use jade - creates an Express global variable
+
+app.locals.title = 'The Coolest Cal App'; // an object passed to every res.render
+// app.set('title', 'The Coolest Cal App');  // this sets a global variable, but you still would need to add 'title' wherever you want it
+
+app.use(bodyParser.urlencoded({ extended: false}));  // this is middleware for the body-parser
+	// the line above adds to the waterfall so it will parse the info from the file and then continue down the waterfall chain
+
+
 app.get('/', (req, res) => {
 	setTimeout(() => {  // could use this to load part of the site later, i.e. had to access database first or an API, for example
 // getData.then(data => { });  // could also use a promise and callback rather than a timer
 	res.render('index', {  // passing object to rendering engine as it passes index.jade
-		title: 'Calendar App That Will Make You Millions',
+		// title: 'Calendar App That Will Make You Millions',  // no longer needed since using 'app.locals.title'
 		date: new Date()
 	});  // by default, will look for a 'views' folder containing 'index.jade'
 }, 1000);
 });
 
-// EXPRESS SYNTAX
-// app.get('/hello', (req, res) => {
-// 		res.write('<h1>Hello World</h1>');  // vanilla node
+app.get('/contact', (req, res) => {
+	if (req.query.name) {
+		res.render('contact');
+	}
 
-// 		// query parameter handling
-// 		// http://localhost:3000/hello/?name=Bob
-// 		const name = req.query.name;
-// 		const msg = `<h1>Hello ${name}</h1>`;  // NOTE use of alt quotes
-// 		console.log('QUERY PARAMS>>>>>', req.query);
-
-
-// 		msg.split('').forEach((char, i) => { // creates an array of characters
-// 		setTimeout (() => {
-// 			res.write(char)  // will now write characters slowly
-// 		}, 500 * i);  // '* i' extends the time for each subsequent character
-// 		});  
-
-// 		setTimeout (() => {  // timer for when pageload will end
-// 			res.end();		 // otherwise, the load will continue to spin
-// 		}, 20000);
-// });
-
-// app.get('/random', (req, res) => {
-// 	res.send(Math.random().toString());
-//   // res.send('Hello World!');
-// });
-
-// app.get('/random/:min/:max', (req, res) => {
-// 	const min = req.params.min;
-// 	const max = req.params.max;
-// });
-
-app.all('*', (req, res) => {  // 'all' means any verb (get, post, delete, etc will then run this)
-	// res.writeHead(403);
-	res
-		.status(403)
-		.send('Access Denied');
-	// res.end('Access Denied');
-  // res.send('Hello World!');
+	res.render('contact');
 });
+
+app.post('/contact', (req, res) => {
+	// debugger;
+	console.log(req.body);
+	const name = req.body.name;
+	res.send('<h1>Thanks for contacting us, ' + name + '</h1>');
+});
+
+// app.get('/hello', (req, res)) => {
+// 	const naem = req.query.name || 'World';
+// 	const msg = 'hello'
+// }
+
+
+// app.all('*', (req, res) => {  // 'all' means any verb (get, post, delete, etc will then run this)
+// 	res
+// 		.status(403)
+// 		.send('Access Denied');
+// });
 
 
 
