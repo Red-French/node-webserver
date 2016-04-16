29bc8a836d3da12c40f4c5bf9a1ab9d7104b3056
Red-French <red@redfrench.net>
2016-04-15 23:45:45 -0500

    modularize (add comments)

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const app = require('express')();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const routes = require('./routes/');
-
-const PORT = process.env.PORT || 3000;
+const routes = require('./routes/routes');  // Always looks for an 'index' file unless you specify
+                                            // Could have renamed 'routes.js' 'index.js' and made
+                                            // this path './routes/'
+const PORT = process.env.PORT || 3000;  // dynamic port or port 3000
 
 const MONGODB_HOST = process.env.MONGODB_HOST || 'localhost';
 const MONGODB_PORT = process.env.MONGODB_PORT || 27017;
@@ -15,10 +16,11 @@ const MONGODB_PASS = process.env.MONGODB_PASS || '';
 const MONGODB_NAME = process.env.MONGODB_NAME || 'reds-node-webserver';
 
 const MONGODB_AUTH = MONGODB_USER
-  ? `${MONGODB_USER}:${MONGODB_PASS}@`
+  ? `${MONGODB_USER}:${MONGODB_PASS}@`  // TERNARY OPERATOR (? :)
   : '';
 
 const MONGODB_URL = `mongodb://${MONGODB_AUTH}${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_NAME}`;
+// protocol://user:pass@host:port/path (this is now common protocol)
 
 app.set('view engine', 'jade');
 
